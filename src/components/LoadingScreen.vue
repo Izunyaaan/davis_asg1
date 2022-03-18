4b435cdac8029362f7da3e633cc7b93392c15158
Izunyaaan <kanebluerivers@gmail.com>
2022-03-18 18:06:59 +0800

    changed selected csv

---
+++ src/components/LoadingScreen.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    style="
+      position: absolute;
+      top: 0;
+      right: 0;
+      width: 100%;
+      height: 100%;
+      z-index: 3;
+    "
+    class="background d-flex justify-start flex-column text-center"
+    v-if="isLoading"
+  >
+    <p class="text-h1 pa-3">Loading</p>
+    <v-spacer
+      ><v-progress-circular
+        indeterminate
+        size="75"
+        class="pa-3"
+      ></v-progress-circular
+    ></v-spacer>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["isLoading"],
+};
+</script>
+
+<style>
+</style>
--- src/views/PreloadedCSV.vue
+++ src/views/PreloadedCSV.vue
@@ -1,83 +1,140 @@
 <template>
   <div class="d-flex flex-wrap">
-    <div
-      style="
-        position: absolute;
-        top: 0;
-        right: 0;
-        width: 100%;
-        height: 100%;
-        z-index: 3;
-      "
-      class="background d-flex justify-center"
-      v-show="isLoading"
-    >
-      <p class="text-h1 pa-3">Loading</p>
-      <v-progress-circular
-        indeterminate
-        size="75"
-        class="pa-3"
-      ></v-progress-circular>
-    </div>
+    <LoadingScreen :isLoading="isLoading" />
+    <h1>Charts of Salary vs Working Experience</h1>
+
+    <!-- Controls -->
     <v-card outlined tile elevation="5" width="33%" min-width="350px">
-      <v-card-title>{{ this.test }}</v-card-title>
-      <Plotly :data="data" :layout="layout" :display-mode-bar="false"></Plotly>
+      <v-card-title>Controls</v-card-title>
+      <v-card-text>controls go here</v-card-text>
     </v-card>
+
+    <!-- Table -->
     <v-card outlined tile elevation="5" width="33%" min-width="350px">
-      <v-card-title>{{ this.test }}</v-card-title>
-      <Plotly :data="data" :layout="layout" :display-mode-bar="false"></Plotly>
+      <v-card-title>Table</v-card-title>
+      <Plotly :data="graphData.table.data" :display-mode-bar="false"></Plotly>
     </v-card>
+
+    <!-- Scatter -->
     <v-card outlined tile elevation="5" width="33%" min-width="350px">
-      <v-card-title>{{ this.test }}</v-card-title>
-      <Plotly :data="data" :layout="layout" :display-mode-bar="false"></Plotly>
+      <v-card-title>Scatter</v-card-title>
+      <!-- <Plotly :data="data" :layout="layout" :display-mode-bar="false"></Plotly> -->
     </v-card>
+
+    <!-- Bar -->
     <v-card outlined tile elevation="5" width="33%" min-width="350px">
-      <v-card-title>{{ this.test }}</v-card-title>
-      <Plotly :data="data" :layout="layout" :display-mode-bar="false"></Plotly>
+      <v-card-title>Histogram</v-card-title>
+      <!-- <Plotly :data="data" :layout="layout" :display-mode-bar="false"></Plotly> -->
     </v-card>
+
+    <!-- Pie -->
     <v-card outlined tile elevation="5" width="33%" min-width="350px">
-      <v-card-title>{{ this.test }}</v-card-title>
-      <Plotly :data="data" :layout="layout" :display-mode-bar="false"></Plotly>
+      <v-card-title>Pie</v-card-title>
+      <!-- <Plotly :data="data" :layout="layout" :display-mode-bar="false"></Plotly> -->
     </v-card>
+
+    <!-- Line -->
     <v-card outlined tile elevation="5" width="33%" min-width="350px">
-      <v-card-title>{{ this.test }}</v-card-title>
-      <Plotly :data="data" :layout="layout" :display-mode-bar="false"></Plotly>
+      <v-card-title>Line</v-card-title>
+      <!-- <Plotly :data="data" :layout="layout" :display-mode-bar="false"></Plotly> -->
     </v-card>
   </div>
 </template>
 
 <script>
 import { Plotly } from "vue-plotly";
+import LoadingScreen from "../components/LoadingScreen.vue";
 
 export default {
   components: {
     Plotly,
+    LoadingScreen,
   },
   created() {
-    // Simple GET request
-    const requestOptions = {
-      method: "GET",
-    };
-    fetch(
-      "https://raw.githubusercontent.com/Izunyaaan/davis_asg1/main/src/assets/data_cleaned_2021.csv",
-      requestOptions
-    )
-      .then((response) => response.text())
-      .then((data) => console.log(data));
+    this.loadCSVData(
+      "https://raw.githubusercontent.com/Izunyaaan/davis_asg1/main/src/assets/Salary.csv"
+    ).then((data) => {
+      this.parseIntoTable(data);
+      this.isLoading = false;
+    });
   },
   data: () => ({
     isLoading: true,
-    data: [
-      {
-        x: [1, 2, 3, 4],
-        y: [10, 15, 13, 17],
-        type: "scatter",
+    graphData: {
+      table: {
+        data: [
+          {
+            type: "table",
+            header: {
+              values: [],
+              align: "center",
+              fill: { color: "grey" },
+              font: { color: "white" },
+            },
+            cells: {
+              values: [],
+              align: "center",
+            },
+          },
+        ],
       },
-    ],
-    layout: {
-      title: "My graph",
-      autosize: true,
+      scatter: {
+        data: [
+          {
+            x: [],
+            y: [],
+            type: "scatter",
+            mode: "markers",
+          },
+        ],
+        layout: {
+          autosize: true,
+          xaxis: {
+            title: {
+              text: "",
+            },
+          },
+          yaxis: {
+            title: {
+              text: "",
+            },
+          },
+        },
+      },
     },
   }),
+  methods: {
+    loadCSVData(url) {
+      // Simple GET request
+      const requestOptions = {
+        method: "GET",
+      };
+      return fetch(url, requestOptions).then((response) => response.text());
+    },
+
+    parseIntoTable(data) {
+      let dataArr = data.split(/\r?\n/);
+      dataArr.forEach((item, index) => {
+        if (index === 0) {
+          //split row
+          let rowArr = item.split(",");
+          //populate header
+          this.graphData.table.data[0].header.values = rowArr;
+          for (let i = 0; i < rowArr.length; i++) {
+            this.graphData.table.data[0].cells.values.push([]);
+          }
+        } else {
+          //populate cells
+          let rowArr = item.split(",");
+          rowArr.forEach((rowItem, rowIndex) => {
+            this.graphData.table.data[0].cells.values[rowIndex].push(rowItem);
+          });
+        }
+      });
+    },
+    parseIntoScatter(data) {
+      console.log(data);
+    },
+  },
 };
 </script>
